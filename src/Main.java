39763cc1f8e833a671c3d3726730f522da587a3e
Martin Palman <martin@ellva.se>
2016-10-31 14:59:30 +0100

    commit from dev branchen, i just added a line in the code, in order to have something to merge

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("This line I added from dev branch");
 		System.out.println("This line was added from master branch");
+		System.out.println("This line i added from dev brunch, just to have something to preform a merge");
 	}
 
 }
